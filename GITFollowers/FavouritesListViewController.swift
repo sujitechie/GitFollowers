f9873a37875083974ca3c8736785b9d7335a5b80
sujith <psujith.92@gmail.com>
2024-09-23 13:28:03 +0530

    Initial commit

---
+++ GITFollowers/FavouritesListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavouritesListViewController.swift
+//  GITFollowers
+//
+//  Created by sujith on 23/09/24.
+//
+
+import UIKit
+
+class FavouritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
